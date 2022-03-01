22b8bd7df8b4bc11513c4d4c3a45c04805a4f896
asdfghjkkl11 <asdfghjkkl11@naver.com>
2022-03-01 21:14:09 +0900

    /record_per 추가

--- routes/http_module.js
+++ routes/http_module.js
@@ -1,23 +1,16 @@
-const http = require('https');
+const axios = require('axios');
 const iconv = require('iconv-lite');
 
 getDataFromHttp = function (url){
-    return new Promise( (resolve, reject) => {
-        http.get(url, (res) => {
-            let body = [];
-
-            res.on('data', function(chunk) {
-                body.push(chunk);
-            });
-
-            res.on('end', function() {
-                let decodeHtml = iconv.decode(Buffer.concat(body), 'euc-kr')
-
-                resolve(decodeHtml);
-            });
-
-        }).on('error', reject);
+    return axios.get(url, { responseType: 'arraybuffer'}).then(function (res){
+       return iconv.decode(res.data, 'euc-kr');
     });
 }
 
+postDataFromHttp = function (url,data){
+    return axios.post(url, data, { responseType: 'arraybuffer'}).then(function (res){
+        return iconv.decode(res.data, 'euc-kr');
+    });
+}
 module.exports.getDataFromHttp = getDataFromHttp;
+module.exports.postDataFromHttp = postDataFromHttp;
