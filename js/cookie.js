88523c45146185f6bfd9054abaea21d988ec7ce5
asdfghjkkl11 <asdfghjkkl11@naver.com>
2022-05-02 19:06:20 +0900

    login 전송방식 변경

--- js/cookie.js
+++ js/cookie.js
@@ -3,11 +3,11 @@ const config = require('./config');
 
 let cookie = 'b906cc9cdd8eebc89ea2100586f82d69';
 let url = config.url + '/login.php';
-let query = {
-    'mstat96': cookie,
-    'passwd': config.password
-};
 
-http.postDataFromHttp(url,query);
+let data = new URLSearchParams();
+data.append('mstat96', cookie);
+data.append('passwd', config.password);
+
+http.postDataFromHttp(url,data);
 
 module.exports = 'mstat96=' + cookie + ';'
