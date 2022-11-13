3384fc482bb7ca43bc0406d662ab4699ab4ab9f1
asdfghjkkl11 <asdfghjkkl11@naver.com>
2022-11-14 00:11:16 +0900

    api 수정

--- js/cookie.js
+++ js/cookie.js
@@ -2,7 +2,7 @@ const http = require('./http_module');
 const config = require('./config');
 
 let cookie = 'b906cc9cdd8eebc89ea2100586f82d69';
-let url = config.url + '/login.php';
+let url = config.url + '96/login.php';
 
 let data = new URLSearchParams();
 data.append('mstat96', cookie);
